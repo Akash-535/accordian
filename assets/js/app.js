0d61dd0c01ba61fcd103332c580912d3046913ee
akash-535 <akashbish9e@gmail.com>
2024-08-03 09:04:04 +0530

    autoplay added on slider-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,11 @@ $(".sliderbox").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: false,
+   
     autoplaySpeed: 2000,
     infinite: true,
     variableWidth: true,
+   
     responsive: [
         {
             breakpoint: 1024,
@@ -50,7 +52,7 @@ $(".sliderbox").slick({
         }
     ]
 });
-
+ 
 $(".sliderboxs").slick({
     dots: true,
     infinite: true,
@@ -60,7 +62,7 @@ $(".sliderboxs").slick({
  
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
     infinite: true,
     variableWidth: true,
@@ -112,4 +114,16 @@ $('.slider-boxthree').slick({
     centerMode: true,
     variableWidth: true
   });
-                  
+  window.sliderOne=function(){
+    $('.sliderboxs').slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"></button>',
+    centerMode:true,
+    slidesToShow:3,
+    slidesToScroll:1
+    });
+  };
+  
